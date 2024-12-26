d1e401e616672dcaa25709a7b7dd6e491fc65bc8
jernejfrank <jernejfrank@gmail.com>
2024-12-26 19:48:00 +0800

    Add app_id to sqlite app

--- burr_agent/chat_sqllite3_db.py
+++ burr_agent/chat_sqllite3_db.py
@@ -8,7 +8,7 @@ from burr.core import Application, persistence
 from burr_agent import app_graph
 
 
-def create_chat_app(user: str) -> Application:
+def create_chat_app(app_id: int, user: str) -> Application:
     """Creating unique chat app for each user."""
     partition_key = f"new_burr_user_{user}"
 
@@ -26,6 +26,6 @@ def create_chat_app(user: str) -> Application:
             default_entrypoint="human_input",
         )
         .with_state_persister(sqllite_persister)
-        .with_identifiers(partition_key=partition_key)
+        .with_identifiers(app_id=str(app_id), partition_key=partition_key)
         .build()
     )
